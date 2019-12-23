e53a3155c6380c5d31e681cd9fb6e6a01e372395
sanelee38 <1870731037@163.com>
2019-12-23 09:37:49 +0800

    modify high search

--- src/main/java/com/sanelee/zhiyuan/Controller/HighSearchController.java
+++ src/main/java/com/sanelee/zhiyuan/Controller/HighSearchController.java
@@ -7,6 +7,7 @@ import com.sanelee.zhiyuan.Mapper.SchoolExtMapper;
 import com.sanelee.zhiyuan.Model.GaoKao;
 import com.sanelee.zhiyuan.Model.Profession;
 import com.sanelee.zhiyuan.Model.School;
+import com.sanelee.zhiyuan.Service.HighSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +28,8 @@ public class HighSearchController {
     private ProfessionExtMapper professionExtMapper;
     @Autowired
     private GaoKaoExtMapper gaoKaoExtMapper;
+    @Autowired
+    private HighSearchService highSearchService;
 
     @RequestMapping("/highSearch")
     public String highSearch(Model model){
@@ -41,21 +44,9 @@ public class HighSearchController {
                          @RequestParam(name = "profession",required = false) String profession,
                          @RequestParam(name = "type",required = false) Integer type
                          ){
-        Integer is211=null;
-        Integer is985=null;
-        Integer isDoubleFirstClass=null;
-        if (type==985){
-            is985=985;
-        }else if(type==211){
-            is211=211;
-        }else if(type==1){
-            isDoubleFirstClass=1;
-        }else {
-            is211=null;
-            is985=null;
-            isDoubleFirstClass=null;
-        }
-        List<GaoKao> schoolSearchList = gaoKaoExtMapper.schoolHighSearch(area,profession,is211,is985,isDoubleFirstClass);
+
+
+        List<GaoKao> schoolSearchList = highSearchService.schoolHighSearch(area,profession,type);
         List<Profession> professionList = professionExtMapper.selectProfession();
         model.addAttribute("professionList",professionList);
         model.addAttribute("schoolList",schoolSearchList);
---
+++ src/main/java/com/sanelee/zhiyuan/Service/HighSearchService.java
@@ -0,0 +1,46 @@
+package com.sanelee.zhiyuan.Service;
+
+import com.sanelee.zhiyuan.Mapper.GaoKaoExtMapper;
+import com.sanelee.zhiyuan.Model.GaoKao;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class HighSearchService {
+    @Autowired
+    private GaoKaoExtMapper gaoKaoExtMapper;
+
+    public List<GaoKao> schoolHighSearch(String area, String profession, Integer type) {
+        if(StringUtils.isNotBlank(area)){
+            String[] tags = StringUtils.split(area,",");
+            area = Arrays.stream(tags).collect(Collectors.joining("|"));
+        }
+        Integer is211=null;
+        Integer is985=null;
+        Integer isDoubleFirstClass=null;
+        if (type==985){
+            is985=985;
+        }else if(type==211){
+            is211=211;
+        }else if(type==1){
+            isDoubleFirstClass=1;
+        }else if(type==0){
+            is211=0;
+            is985=0;
+            isDoubleFirstClass=0;
+        }else if(type==-1){
+            is211=null;
+            is985=null;
+            isDoubleFirstClass=null;
+        }
+
+        List<GaoKao> schoolSearchList = gaoKaoExtMapper.schoolHighSearch(area,profession,is211,is985,isDoubleFirstClass);
+        return schoolSearchList;
+    }
+
+}
